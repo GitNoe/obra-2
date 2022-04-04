623baa5a500052d70ffd0d854be07bdf432d9034
Noelia Figueiras Fernández <noelia.figueiras@vigo.org>
2022-04-04 12:21:09 +0200

    abril

--- persoas/index.php
+++ persoas/index.php
@@ -125,7 +125,7 @@ if ($stmt = $mysqli->prepare('SELECT * FROM persoas ORDER BY id LIMIT ?,?')) {
               <!-- <td><a href="#"><i class="fa-solid fa-eye"></i></a></td>
                 <td><a href="functions/modificar.php?id=<?php echo $row['id']; ?>"><i class="fas fa-pencil-alt"></i></a></td>
                 <td><a href="functions/eliminar.php?id=<?php echo $row['id'] ?>"><i class="fas fa-trash-alt"></i></a></td> -->
-                <td><a class="red-icons" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                <td><a class="red-icons" href="functions/ver_ficha.php?id=<?php echo $row['id']; ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
                     <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
                     <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
                   </svg></a>
@@ -185,4 +185,3 @@ if ($stmt = $mysqli->prepare('SELECT * FROM persoas ORDER BY id LIMIT ?,?')) {
 </body>
 
 </html>
-<!-- <?php $stmt->close(); ?> -->
